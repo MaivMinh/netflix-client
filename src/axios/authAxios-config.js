7a81b330c5ceb77618e8bcf18687040baf07c38e
MaivMinh <maivanminh.se@gmail.com>
2024-06-10 09:44:26 +0700

    Change port from 8080 to 4000

--- src/axios/authAxios-config.js
+++ src/axios/authAxios-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 const authAxios = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:4000",
   withCredentials: true,
   headers: {
     "Content-Type": "Application/json",
